33900ee0aa610fd1604dca5447e489f8b6ad8042
Lewis Odero <lewisodero27@gmail.com>
2024-08-03 01:58:23 +0300

    feat: add functionalities for fetching of data,handle add transaction and filter of transaction base on description

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  return (
-    <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
-    </div>
-  );
-}
-
-export default AccountContainer;
+  const [transactions, setTransactions] = useState([]);
+  const [searchDes, setSearchDes] = useState("");
+  
+  useEffect(() => {
+     fetch("http://localhost:8001/transactions")
+       .then(response => response.json())
+       .then(data => setTransactions(data));
+   }, []);
+  
+  const handleAddTransaction = newTransaction => {
+      setTransactions([...transactions, newTransaction]);
+    };
+  
+    const filteredTransactions = transactions.filter(transaction =>
+      transaction.description.toLowerCase().includes(searchDes.toLowerCase())
+    );
+  
+    return (
+      <div>
+        <Search searchDes={searchDes} setSearchDes={setSearchDes} />
+        <AddTransactionForm onAddTransaction={handleAddTransaction} />
+        <TransactionsList transactions={filteredTransactions} />
+      </div>
+    );
+  }
+  
+  export default AccountContainer;
