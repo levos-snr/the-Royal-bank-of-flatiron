1b5d6f9f29fc161b43e06b2a96ef8b119b3ab680
Lewis Odero <lewisodero27@gmail.com>
2024-08-03 02:28:52 +0300

    feat: update the search logic to trigger the toast notifications using toast.promise and Update the Search component to trigger toast notifications when the search is initiated, successful, or fails.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
-function Search({ searchDes, setSearchDes }) {
+function Search({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = e => {
+    setSearchTerm(e.target.value);
+
+    const searchPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (e.target.value) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, 1000);
+    });
+
+    toast.promise(
+      searchPromise,
+      {
+        loading: 'Searching...',
+        success: <b>Done!</b>,
+        error: <b>Not Found.</b>,
+      }
+    );
+
+    onSearch(e.target.value);
+  };
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        value={searchDes}
-        onChange={e => setSearchDes(e.target.value)}
+        value={searchTerm}
+        onChange={handleSearch}
       />
       <i className="circular search link icon"></i>
     </div>
