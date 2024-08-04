8b4b0d48a65781c77a522248a4ed406c83aaa60b
Lewis Odero <lewisodero27@gmail.com>
2024-08-05 00:17:09 +0300

    feat: update the final push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Header, Icon, Menu } from 'semantic-ui-react';
+import React from "react";
+import { Header, Icon, Menu } from "semantic-ui-react";
 
 const HeaderComponent = () => {
   return (
-    <Menu className='ui violet inverted pointer'>
-      <Menu.Item className='link'>
-        <Header as='h1' inverted>
-          <Icon name='dollar sign' />
+    <Menu className="ui violet inverted pointer">
+      <Menu.Item className="link">
+        <Header as="h1" inverted>
+          <Icon name="dollar sign" />
           The Royal Bank of Flatiron
         </Header>
       </Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item className='link'>
-          <Icon name='home' />
+      <Menu.Menu position="right">
+        <Menu.Item className="link">
+          <Icon name="home" />
           Home
         </Menu.Item>
-        <Menu.Item className='link'>
-          <Icon name='file alternate' />
+        <Menu.Item className="link">
+          <Icon name="file alternate" />
           Reports
         </Menu.Item>
-        <Menu.Item className='link'>
-          <Icon name='user' />
+        <Menu.Item className="link">
+          <Icon name="user" />
           Profile
         </Menu.Item>
       </Menu.Menu>
